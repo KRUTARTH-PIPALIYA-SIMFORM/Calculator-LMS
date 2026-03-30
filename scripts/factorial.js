98ea9a8b5f14511f540f105ec1e26764c9eba939
Krutarth Pipaliya <krutarth.pipaliya@simformsolutions.com>
2026-03-30 15:10:31 +0530

    feat: add factorial logic

---
+++ scripts/factorial.js
@@ -0,0 +1,10 @@
+export default function factorial(n) {
+    if (n < 0) return NaN;
+    n = BigInt(n);
+    let ans = n;
+    while (--n) {
+        ans *= n;
+    }
+    console.log(ans);
+    return ans;
+}
